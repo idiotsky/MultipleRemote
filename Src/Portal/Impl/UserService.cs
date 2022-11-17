b91234d6666a454b536bcdec12f392a3f49ad18e
yan_zheng <yan.zheng@infoflosolutions.com>
2022-11-17 12:22:40 -0500

    feat:portal add initial sample

---
+++ Src/Portal/Impl/UserService.cs
@@ -0,0 +1,16 @@
+﻿using Interface;
+
+namespace Impl
+{
+    public class UserService : IUserService
+    {
+        public IList<User> LoadAll()
+        {
+            return new List<User>
+                   {
+                       new() { FirstName = "Tom", LastName = "Blue" },
+                       new() { FirstName = "Jerry", LastName = "Green" }
+                   };
+        }
+    }
+}
---
+++ Src/Portal/Interface/IUserService.cs
@@ -0,0 +1,6 @@
+namespace Interface;
+
+public interface IUserService
+{
+    IList<User> LoadAll();
+}
